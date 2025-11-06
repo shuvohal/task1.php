39cebcb1e4d5889efa64f191c3ccdf9208f783a7
shuvo halder <shuvohalder4545@gmail.com>
2025-11-06 22:57:22 +0600

    practice

--- practice.php
+++ practice.php
@@ -1,17 +1,2 @@
 
-<?php   
-//1. Create constants for app name and author
 
-define("app_name","human");
-define("author","shuvo");
-
-//2. Print constants
-
-echo app_name ."<br>";
-print author ."<br>";
-printf("applicatioon:%s | author:%s .<br>",app_name,author);
-
-
-
-
-?>
